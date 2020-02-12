80821de6a22f3d634640d5ca658d12fc3470e07f
yassine yakine <yakineyassine@outlook.fr>
2020-02-12 11:46:39 +0100

    Ajoutez des fichiers projet.

---
+++ DevStudios.Ormy/OrmyContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStudios.Ormy
+{
+    public class OrmyContext
+    {
+        private readonly DbConnection _connection;
+        public OrmyContext(DbConnection connection)
+        {
+            _connection = connection;
+        }
+    }
+}
